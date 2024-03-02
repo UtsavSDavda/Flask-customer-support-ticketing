952ce79c3f1cf7e655dba3681500e1cd122dddd3
UtsavSDavda <davdautsav42@gmail.com>
2024-03-02 20:44:44 +0530

    create first codes

--- app.py
+++ app.py
@@ -1,20 +1,41 @@
 import pickle
 from flask import Flask, render_template, request, redirect, url_for, session
 import requests
-import numpy as np
-import pandas as pd
-import matplotlib
 import datetime
 import boto3
 app = Flask(__name__)
 
 @app.route("/")
 def intro():
-    return "HI BRO"
-@app.route("/Hibro")
-@app.route("/Hibro/<name>")
-def name(name=None):
-    return render_template("myworld.html",name=name)
+    return render_template("myworld.html")
+@app.route("/loguser", methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("myworld.html")
+    elif request.method == 'POST':
+        name1 = request.form('username')
+        pass1 = request.form('password')
+        response = table.get_item(Key={'username': username})
+        if 'Item' not in response:
+            return "Invalid username or password"
+        hashed_password = response['Item']['password']
+        if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
+            return render_template("user.html")
+        else:
+            return "Invalid username or password"
+
+#@app.route("/logout")
+#def logout():
+
+#@app.route("/main")
+#def main():
+
+#@app.route("/employee")
+#def employee():
+
+#@app.route("/user")
+#def user():
+
 
 
 
